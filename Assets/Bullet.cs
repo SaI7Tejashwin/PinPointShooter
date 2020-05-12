109db7042b868a01958c54eb75dbeb4bc7731cf6
SaI7Tejashwin <57253627+SaI7Tejashwin@users.noreply.github.com>
2020-05-12 05:59:49 +0530

    Merge branch 'master' of https://github.com/SaI7Tejashwin/PinPointShooter

---
+++ Assets/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    private float y_limit = 5;
+    private float x_limit = 8;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y > y_limit || transform.position.y < -y_limit || transform.position.x > x_limit || transform.position.x < -x_limit)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 20f;
+    private Rigidbody2D playerRb;
+    public Camera cam;
+
+    Vector2 move;
+    Vector2 mousePos;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move.x = Input.GetAxisRaw("Horizontal");
+        move.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+       
+
+        
+    }
+
+
+    private void FixedUpdate()  //this is used when you desire for smooth physics movement.
+    {
+
+
+        playerRb.MovePosition(playerRb.position + move * speed * Time.fixedDeltaTime);
+
+        Vector2 lookDirection = mousePos - playerRb.position;
+
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+
+        playerRb.rotation = angle;
+
+
+    }
+}
