a0f8c2b0bedb87d81806387c2f09d74bebfdf987
SaI7Tejashwin <57253627+SaI7Tejashwin@users.noreply.github.com>
2020-05-14 21:34:57 +0530

    Added slight changes to shooting mech and audio sample

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -5,24 +5,35 @@ using UnityEngine;
 public class Shooting : MonoBehaviour
 {
 
-    public Transform firePoint;
+    
     public GameObject bulletPrefab;
-
+    public Transform firePoint;
     public float bulletForce = 20f;
-   
-    // Start is called before the first frame update
-    void Start()
+
+    public float fireRate = 0.001f;
+
+    public AudioClip laser;
+    private AudioSource audioSrc;
+    
+    [SerializeField]private float nextFire = 0.0f;
+
+
+     void Start()
     {
-        
+        audioSrc = GetComponent<AudioSource>();   
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
         {
+            nextFire = Time.time + fireRate;
+
+            audioSrc.PlayOneShot(laser, 1.0f);
+
             Shoot();
-        }
+        }     
 
        
     }
@@ -31,11 +42,9 @@ public class Shooting : MonoBehaviour
     {
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
 
-        Rigidbody2D bulletRb = bullet.GetComponent<Rigidbody2D>();
+       Rigidbody2D bulletRb = bullet.GetComponent<Rigidbody2D>();
 
         bulletRb.AddForce(firePoint.right * bulletForce, ForceMode2D.Impulse);
-
-   
 
     }
 }
---
+++ Assets/Sprites/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    private float y_limit = 5;
+    private float x_limit = 8;
+    
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        if (transform.position.y > y_limit || transform.position.y < -y_limit || transform.position.x > x_limit || transform.position.x < -x_limit)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
